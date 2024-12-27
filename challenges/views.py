827fea461ed7345793fdff129c1dda9c1c784610
smoaking90 <cwsmoak90@gmail.com>
2024-12-27 18:56:38 +0100

    django template language

--- challenges/views.py
+++ challenges/views.py
@@ -17,6 +17,7 @@ monthly_challenges = {
     'december': 'Learn PHP.'
 }
 
+
 # Create your views here.
 
 def index(request):
@@ -30,6 +31,7 @@ def index(request):
     response_data = f'<ul>{list_items}</ul>'
     return HttpResponse(response_data)
 
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -37,12 +39,16 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound('Invalid Month.')
 
     redirect_month = months[month - 1]
-    redirect_path = reverse('month-challenge', args=[redirect_month]) # /challenge/january
+    redirect_path = reverse('month-challenge', args=[redirect_month])  # /challenge/january
     return HttpResponseRedirect(redirect_path)
+
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, 'challenges/challenge.html')
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text,
+            'month_name': month.capitalize()
+        })
     except:
         return HttpResponseNotFound('<h1>Month not found.</h1>')
